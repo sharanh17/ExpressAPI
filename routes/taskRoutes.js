3a3e66c8d6636ab7bb0ea6655d47fd20adf7f90b
sharanh17 <sharanhidaduggi17@gmail.com>
2024-05-15 11:27:39 +0530

    slash added

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,6 +4,9 @@ const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController');
 
+router.get('/', (req, res) => {
+    res.send('Welcome to the API');
+});
 router.post('/tasks', taskController.createTask);
 router.get('/tasks', taskController.getAllTasks);
 router.put('/tasks/:id', taskController.updateTask);
